ef62fbe1d05e3f006a6ab05a8efdd12a3a5710a5
elnexreal <lolopundang1@gmail.com>
2022-12-31 17:26:27 -0300

    ui created, open button function,

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GD-SLEditor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Geometry Dash server link editor.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GD-SLEditor")]
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
