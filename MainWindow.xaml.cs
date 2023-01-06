3605269a061923d50b35476ef318f25abde1d0fa
elnexreal <40328573+elnexreal@users.noreply.github.com>
2023-01-06 18:11:17 -0300

    Trying to add HexModify()

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿//using System;
+﻿using System.IO;
 //using System.Collections.Generic;
 //using System.Linq;
-//using System.Text;
+using System.Text;
 //using System.Threading.Tasks;
+using System;
 using System.Windows;
 using System.Windows.Forms;
+using GD_SLEditor.Handling;
 //using System.Windows.Data;
 //using System.Windows.Documents;
 //using System.Windows.Input;
@@ -23,8 +25,10 @@ namespace GD_SLEditor
 		string GDPath;
 		string Server;
 
+		// Open button function
 		private void OpenFileMenu(object sender, RoutedEventArgs e)
 		{
+			// Select file dialog config
 			OpenFileDialog dialog = new OpenFileDialog
 			{
 				Multiselect = false,
@@ -35,6 +39,7 @@ namespace GD_SLEditor
 				Filter = "Geometry Dash Executable|GeometryDash.exe"
 			};
 
+			// Check if user cancelled file selection
 			if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
 			{
 				GDPath = dialog.FileName;
@@ -42,19 +47,37 @@ namespace GD_SLEditor
 			} 
 			else
 			{
-				System.Windows.MessageBox.Show("Please select your Geometry Dash executable", "Error", MessageBoxButton.OK);
+				System.Windows.MessageBox.Show("Please select your Geometry Dash executable", "Operation cancelled", MessageBoxButton.OK);
 			}
 		}
 
-		// WIP
-		private void HexModify()
+		private void ApplyButton(object sender, RoutedEventArgs e)
 		{
 			Server = Serverlink.Text;
+			if (Server.Length == 33)
+			{
+				try
+				{
+					HexHandler.HexModify(GDPath, FileMode.Open, Server);
+				}
+				catch (IOException err)
+				{
+					System.Windows.MessageBox.Show(err.Message);
+				}
+			}
+			else
+			{
+				System.Windows.MessageBox.Show("Your server URL must have exactly 33 characters", "Warning", MessageBoxButton.OK);
+			}
 		}
 
 		private void InitProgram()
 		{
+			// Link the button click to functions
 			Openbtn.Click += OpenFileMenu;
+			Applybtn.Click += ApplyButton;
+
+			Devname.Text += $" - {DateTime.Now.Year}";
 		}
 
 		public MainWindow()
