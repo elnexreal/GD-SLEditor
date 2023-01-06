9d325d6b96abcb84d4b67530215fa94c5052004c
elnexreal <40328573+elnexreal@users.noreply.github.com>
2023-01-06 18:11:17 -0300

    Trying to add HexModify()

---
+++ HexHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace GD_SLEditor.Handling
+{
+	/// <summary>
+	/// Handles the Hex modifications for executables
+	/// </summary>
+	internal class HexHandler
+	{
+		public static void HexModify(string file, FileMode mode, string serverurl)
+		{
+			
+		}
+	}
+}
