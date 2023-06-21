61fe8aacc420c3249bb52d4f7f8fbddfd2b3cdc1
elnexreal <40328573+elnexreal@users.noreply.github.com>
2023-06-21 05:16:39 -0300

    first working version

--- HexHandler.cs
+++ HexHandler.cs
@@ -1,20 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text;
 
 namespace GD_SLEditor.Handling
 {
-	/// <summary>
-	/// Handles the Hex modifications for executables
-	/// </summary>
-	internal class HexHandler
-	{
-		public static void HexModify(string file, FileMode mode, string serverurl)
-		{
-			
-		}
-	}
+    /// <summary>
+    /// Handles the Hex modifications for the executable
+    /// </summary>
+    internal class HexHandler
+    {
+        public static void HexModify(string executable, string serverurl)
+        {
+            string baseUrl = "http://www.boomlings.com/database";
+
+            // Reads the hex of the executable
+            byte[] execBytes = File.ReadAllBytes(executable);
+            string exHex = bytesToHex(execBytes);
+
+            // Reads the hex of the server url provided
+            byte[] urlBytes = Encoding.ASCII.GetBytes(serverurl);
+            string urlHex = bytesToHex(urlBytes);
+
+            // Converts boomlings URL to HEX
+            string boomlingsHex = stringToHex(baseUrl);
+
+            // Base64 encode everything
+            string b64Url = stringToHex(base64encode(serverurl));
+            string b64Boomlings = stringToHex(base64encode(baseUrl));
+
+            // Replace the URL's
+            string encodedHex = exHex.Replace(boomlingsHex, urlHex).Replace(b64Boomlings, b64Url);
+
+            // Write the file
+            File.WriteAllBytes(executable, StringToByteArray(encodedHex));
+
+            // Debug
+            //File.WriteAllText("C:/Users/elnexreal/Desktop/Deb/Gamehex.txt", exHex);
+            //File.WriteAllText("C:/Users/elnexreal/Desktop/Deb/Urlhex.txt", urlHex);
+        }
+
+        // Converts HEX string into a Byte arraw to write in the exe
+        private static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        // Encodes any string given to it
+        private static string base64encode(string url)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(url);
+            return Convert.ToBase64String(bytes);
+        }
+
+        // Converts bytes to HEX strings
+        private static string bytesToHex(byte[] bytes)
+        {
+            return BitConverter.ToString(bytes).Replace("-", string.Empty);
+        }
+
+        // Convert string to hex
+        private static string stringToHex(string input)
+        {
+            return BitConverter.ToString(Encoding.ASCII.GetBytes(input)).Replace("-", string.Empty);
+        }
+    }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,10 @@
-﻿using System.IO;
+﻿using GD_SLEditor.Handling;
+using System.IO;
 //using System.Collections.Generic;
 //using System.Linq;
-using System.Text;
 //using System.Threading.Tasks;
-using System;
 using System.Windows;
-using System.Windows.Forms;
-using GD_SLEditor.Handling;
+using forms = System.Windows.Forms;
 //using System.Windows.Data;
 //using System.Windows.Documents;
 //using System.Windows.Input;
@@ -17,73 +15,74 @@ using GD_SLEditor.Handling;
 
 namespace GD_SLEditor
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		string GDPath;
-		string Server;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string executable;
 
-		// Open button function
-		private void OpenFileMenu(object sender, RoutedEventArgs e)
-		{
-			// Select file dialog config
-			OpenFileDialog dialog = new OpenFileDialog
-			{
-				Multiselect = false,
-				CheckFileExists = true,
-				CheckPathExists = true,
-				AddExtension = true,
-				Title = "Locate your Geometry Dash.exe file",
-				Filter = "Geometry Dash Executable|GeometryDash.exe"
-			};
+        // Open button function
+        private void OpenFileMenu(object sender, RoutedEventArgs e)
+        {
+            // Create a menu that lets you select the executable
+            forms.OpenFileDialog dialog = new forms.OpenFileDialog
+            {
+                Multiselect = false,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                AddExtension = true,
+                Title = "Locate your Geometry Dash.exe file",
+                Filter = "Geometry Dash Executable|*.exe"
+            };
 
-			// Check if user cancelled file selection
-			if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-			{
-				GDPath = dialog.FileName;
-				FPath.Text = GDPath;
-			} 
-			else
-			{
-				System.Windows.MessageBox.Show("Please select your Geometry Dash executable", "Operation cancelled", MessageBoxButton.OK);
-			}
-		}
+            // Check if user selected a file
+            if (dialog.ShowDialog() == forms.DialogResult.OK)
+            {
+                executable = dialog.FileName;
+                FPath.Text = executable;
 
-		private void ApplyButton(object sender, RoutedEventArgs e)
-		{
-			Server = Serverlink.Text;
-			if (Server.Length == 33)
-			{
-				try
-				{
-					HexHandler.HexModify(GDPath, FileMode.Open, Server);
-				}
-				catch (IOException err)
-				{
-					System.Windows.MessageBox.Show(err.Message);
-				}
-			}
-			else
-			{
-				System.Windows.MessageBox.Show("Your server URL must have exactly 33 characters", "Warning", MessageBoxButton.OK);
-			}
-		}
+                MessageBox.Show("Make sure you have a backup of your GD executable", "Warning", MessageBoxButton.OK);
+            }
+            // Otherwise show a messagebox
+            else
+            {
+                MessageBox.Show("Please select your Geometry Dash executable", "Operation cancelled", MessageBoxButton.OK);
+            }
+        }
+        private void ApplyButton(object sender, RoutedEventArgs e)
+        {
+            string server = Serverlink.Text;
+            if (server.Length == 33)
+            {
+                try
+                {
+                    HexHandler.HexModify(executable, server);
 
-		private void InitProgram()
-		{
-			// Link the button click to functions
-			Openbtn.Click += OpenFileMenu;
-			Applybtn.Click += ApplyButton;
+                    MessageBox.Show("Server url changed to" + server);
+                }
+                catch (IOException err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Your server URL must have exactly 33 characters", "Warning", MessageBoxButton.OK);
+            }
+        }
 
-			Devname.Text += $" - {DateTime.Now.Year}";
-		}
+        private void InitProgram()
+        {
+            // Link the button click to functions
+            Openbtn.Click += OpenFileMenu;
+            Applybtn.Click += ApplyButton;
+        }
 
-		public MainWindow()
-		{
-			InitializeComponent();
-			InitProgram();
-		}
-	}
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitProgram();
+        }
+    }
 }
